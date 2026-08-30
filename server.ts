6924eaf304532be53c0ad97e900ddbddfcef9b79
samarthgad68 <samarth.gad68@gmail.com>
2026-08-30 15:48:32 +0530

    update

--- server.ts
+++ server.ts
@@ -468,7 +468,7 @@ async function startServer() {
     });
   }
 
-  const PORT = 3000;
+  const PORT = Number(process.env.PORT) || 3000;
 
   app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on http://0.0.0.0:${PORT}`);
